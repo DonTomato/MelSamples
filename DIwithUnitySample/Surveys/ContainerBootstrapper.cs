02304a5516f18414e7c44d2cd415d55e8f30fdfc
Michael Voronoff <michael.voronov@gmail.com>
2013-11-13 15:24:49 +0400

    formatting

--- DIwithUnitySample/Surveys/ContainerBootstrapper.cs
+++ DIwithUnitySample/Surveys/ContainerBootstrapper.cs
@@ -22,79 +22,51 @@ using System.Diagnostics;
 
 namespace Surveys
 {
-  public class ContainerBootstrapper
-  {
-    public static void RegisterTypes(IUnityContainer container)
-    {
-      Trace.WriteLine(string.Format("Called RegisterTypes in ContainerBootstrapper"), "UNITY");
+	public class ContainerBootstrapper
+	{
+		public static void RegisterTypes(IUnityContainer container)
+		{
+			Trace.WriteLine(string.Format("Called RegisterTypes in ContainerBootstrapper"), "UNITY");
 
-      var storageAccountType = typeof(StorageAccount);
-      var retryPolicyFactoryType = typeof(IRetryPolicyFactory);
+			var storageAccountType = typeof(StorageAccount);
+			var retryPolicyFactoryType = typeof(IRetryPolicyFactory);
 
-      // Instance registration
-      StorageAccount account = 
-        ApplicationConfiguration.GetStorageAccount("DataConnectionString");
-      container.RegisterInstance(account);
+			// Instance registration
+			StorageAccount account = ApplicationConfiguration.GetStorageAccount("DataConnectionString");
+			container.RegisterInstance(account);
 
-      // Register factories
-      container
-        .RegisterInstance<IRetryPolicyFactory>(
-          new ConfiguredRetryPolicyFactory())
-        .RegisterType<ISurveyAnswerContainerFactory,
-          SurveyAnswerContainerFactory>(
-            new ContainerControlledLifetimeManager());
+			// Register factories
+			container
+				.RegisterInstance<IRetryPolicyFactory>(new ConfiguredRetryPolicyFactory())
+				.RegisterType<ISurveyAnswerContainerFactory, SurveyAnswerContainerFactory>(new ContainerControlledLifetimeManager());
 
-      // Register table types
-      container
-        .RegisterType<IDataTable<SurveyRow>, DataTable<SurveyRow>>(
-          new InjectionConstructor(storageAccountType,
-            retryPolicyFactoryType, Constants.SurveysTableName))
-        .RegisterType<IDataTable<QuestionRow>, DataTable<QuestionRow>>(
-          new InjectionConstructor(storageAccountType,
-            retryPolicyFactoryType, Constants.QuestionsTableName));
+			// Register table types
+			container
+				.RegisterType<IDataTable<SurveyRow>, DataTable<SurveyRow>>(new InjectionConstructor(storageAccountType, retryPolicyFactoryType, Constants.SurveysTableName))
+				.RegisterType<IDataTable<QuestionRow>, DataTable<QuestionRow>>(new InjectionConstructor(storageAccountType, retryPolicyFactoryType, Constants.QuestionsTableName));
 
-      // Register message queue type, use typeof with open generics
-      container
-        .RegisterType(
-            typeof(IMessageQueue<>),
-            typeof(MessageQueue<>),
-            new InjectionConstructor(storageAccountType,
-              retryPolicyFactoryType, typeof(String)));
+			// Register message queue type, use typeof with open generics
+			container
+				.RegisterType(typeof(IMessageQueue<>), typeof(MessageQueue<>), new InjectionConstructor(storageAccountType, retryPolicyFactoryType, typeof(String)));
 
-      // Register blob types
-      container
-        .RegisterType<IBlobContainer<List<string>>,
-          EntitiesBlobContainer<List<string>>>(
-            new InjectionConstructor(storageAccountType,
-              retryPolicyFactoryType, Constants.SurveyAnswersListsBlobName))
-        .RegisterType<IBlobContainer<Tenant>,
-          EntitiesBlobContainer<Tenant>>(
-            new InjectionConstructor(storageAccountType,
-              retryPolicyFactoryType, Constants.TenantsBlobName))
-        .RegisterType<IBlobContainer<byte[]>,
-          FilesBlobContainer>(
-            new InjectionConstructor(storageAccountType,
-              retryPolicyFactoryType, Constants.LogosBlobName, "image/jpeg"))
-        .RegisterType<IBlobContainer<SurveyAnswer>,
-          EntitiesBlobContainer<SurveyAnswer>>(
-            new InjectionConstructor(storageAccountType,
-              retryPolicyFactoryType, typeof(String)));
+			// Register blob types
+			container
+				.RegisterType<IBlobContainer<List<string>>, EntitiesBlobContainer<List<string>>>(new InjectionConstructor(storageAccountType, retryPolicyFactoryType, Constants.SurveyAnswersListsBlobName))
+				.RegisterType<IBlobContainer<Tenant>, EntitiesBlobContainer<Tenant>>(new InjectionConstructor(storageAccountType, retryPolicyFactoryType, Constants.TenantsBlobName))
+				.RegisterType<IBlobContainer<byte[]>, FilesBlobContainer>(new InjectionConstructor(storageAccountType, retryPolicyFactoryType, Constants.LogosBlobName, "image/jpeg"))
+				.RegisterType<IBlobContainer<SurveyAnswer>, EntitiesBlobContainer<SurveyAnswer>>(new InjectionConstructor(storageAccountType, retryPolicyFactoryType, typeof(String)));
 
-      // Register store types
-      container
-        .RegisterType<ISurveyStore, SurveyStore>()
-        .RegisterType<ITenantStore, TenantStore>()
-        .RegisterType<ISurveyAnswerStore, SurveyAnswerStore>(
-          new InjectionFactory((c, t, s) => new SurveyAnswerStore(
-            container.Resolve<ITenantStore>(),
-            container.Resolve<ISurveyAnswerContainerFactory>(),
-            container.Resolve<IMessageQueue<SurveyAnswerStoredMessage>>(
-              new ParameterOverride(
-                "queueName", Constants.StandardAnswerQueueName)),
-            container.Resolve<IMessageQueue<SurveyAnswerStoredMessage>>(
-              new ParameterOverride(
-                "queueName", Constants.PremiumAnswerQueueName)),
-            container.Resolve<IBlobContainer<List<String>>>())));
-    }
-  }
+			// Register store types
+			container
+				.RegisterType<ISurveyStore, SurveyStore>()
+				.RegisterType<ITenantStore, TenantStore>()
+				.RegisterType<ISurveyAnswerStore, SurveyAnswerStore>(new InjectionFactory((c, t, s) => 
+					new SurveyAnswerStore(
+						container.Resolve<ITenantStore>(), 
+						container.Resolve<ISurveyAnswerContainerFactory>(), 
+						container.Resolve<IMessageQueue<SurveyAnswerStoredMessage>>(new ParameterOverride("queueName", Constants.StandardAnswerQueueName)),
+						container.Resolve<IMessageQueue<SurveyAnswerStoredMessage>>(new ParameterOverride("queueName", Constants.PremiumAnswerQueueName)),
+						container.Resolve<IBlobContainer<List<String>>>())));
+		}
+	}
 }
