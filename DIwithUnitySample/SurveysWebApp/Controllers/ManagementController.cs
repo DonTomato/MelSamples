15b0fdd8d1f49e10f58a594bf1416d01b2819657
Michael Voronoff <michael.voronov@gmail.com>
2013-11-14 13:03:04 +0400

    formatting

--- DIwithUnitySample/SurveysWebApp/Controllers/ManagementController.cs
+++ DIwithUnitySample/SurveysWebApp/Controllers/ManagementController.cs
@@ -20,32 +20,32 @@ using SurveysWebApp.Models;
 
 namespace SurveysWebApp.Controllers
 {
-    public class ManagementController : Controller
-    {
-      private readonly ITenantStore tenantStore;
+	public class ManagementController : Controller
+	{
+		private readonly ITenantStore tenantStore;
 
-      public ManagementController(ITenantStore tenantStore)
-      {
-        this.tenantStore = tenantStore;
-      }
+		public ManagementController(ITenantStore tenantStore)
+		{
+			this.tenantStore = tenantStore;
+		}
 
-      public ActionResult Index()
-      {
-        var model = new TenantPageViewData<IEnumerable<string>>(this.tenantStore.GetTenantNames())
-        {
-          Title = "Subscribers"
-        };
-        return this.View(model);
-      }
+		public ActionResult Index()
+		{
+			var model = new TenantPageViewData<IEnumerable<string>>(this.tenantStore.GetTenantNames())
+			{
+				Title = "Subscribers"
+			};
+			return this.View(model);
+		}
 
-      public ActionResult Detail(string tenant)
-      {
-        var contentModel = this.tenantStore.GetTenant(tenant);
-        var model = new TenantPageViewData<Tenant>(contentModel)
-        {
-          Title = string.Format("{0} details", contentModel.Name)
-        };
-        return this.View(model);
-      }
-    }
+		public ActionResult Detail(string tenant)
+		{
+			var contentModel = this.tenantStore.GetTenant(tenant);
+			var model = new TenantPageViewData<Tenant>(contentModel)
+			{
+				Title = string.Format("{0} details", contentModel.Name)
+			};
+			return this.View(model);
+		}
+	}
 }
--- DIwithUnitySample/SurveysWebApp/Controllers/SurveysController.cs
+++ DIwithUnitySample/SurveysWebApp/Controllers/SurveysController.cs
@@ -21,80 +21,80 @@ using SurveysWebApp.Models;
 
 namespace SurveysWebApp.Controllers
 {
-    public class SurveysController : Controller
-    {
-        private readonly ISurveyStore surveyStore;
-        private readonly ISurveyAnswerStore surveyAnswerStore;
-        private readonly ITenantStore tenantStore;
+	public class SurveysController : Controller
+	{
+		private readonly ISurveyStore surveyStore;
+		private readonly ISurveyAnswerStore surveyAnswerStore;
+		private readonly ITenantStore tenantStore;
 
-        public SurveysController(
-          ISurveyStore surveyStore,
-          ISurveyAnswerStore surveyAnswerStore,
-          ITenantStore tenantStore)
-        {
-          this.surveyStore = surveyStore;
-          this.surveyAnswerStore = surveyAnswerStore;
-          this.tenantStore = tenantStore;
-        }
+		public SurveysController(
+		  ISurveyStore surveyStore,
+		  ISurveyAnswerStore surveyAnswerStore,
+		  ITenantStore tenantStore)
+		{
+			this.surveyStore = surveyStore;
+			this.surveyAnswerStore = surveyAnswerStore;
+			this.tenantStore = tenantStore;
+		}
 
-        public ITenantStore TenantStore
-        {
-          get { return this.tenantStore; }
-        }
+		public ITenantStore TenantStore
+		{
+			get { return this.tenantStore; }
+		}
 
-        public Tenant Tenant { get; set; }
+		public Tenant Tenant { get; set; }
 
-        [HttpGet]
-        public ActionResult Index(string tenant)
-        {
-          this.Tenant = this.tenantStore.GetTenant(tenant);
+		[HttpGet]
+		public ActionResult Index(string tenant)
+		{
+			this.Tenant = this.tenantStore.GetTenant(tenant);
 
-          IEnumerable<Survey> surveysForTenant = this.surveyStore.GetSurveysByTenant(tenant);
+			IEnumerable<Survey> surveysForTenant = this.surveyStore.GetSurveysByTenant(tenant);
 
-          var model = this.CreateTenantPageViewData(surveysForTenant);
-          model.Title = "My Surveys";
+			var model = this.CreateTenantPageViewData(surveysForTenant);
+			model.Title = "My Surveys";
 
-          return this.View(model);
-        }
+			return this.View(model);
+		}
 
-        [HttpGet]
-        public ActionResult BrowseResponses(string tenant, string surveySlug, string answerId)
-        {
-          this.Tenant = this.tenantStore.GetTenant(tenant);
+		[HttpGet]
+		public ActionResult BrowseResponses(string tenant, string surveySlug, string answerId)
+		{
+			this.Tenant = this.tenantStore.GetTenant(tenant);
 
-          SurveyAnswer surveyAnswer = null;
-          if (string.IsNullOrEmpty(answerId))
-          {
-            answerId = this.surveyAnswerStore.GetFirstSurveyAnswerId(tenant, surveySlug);
-          }
+			SurveyAnswer surveyAnswer = null;
+			if (string.IsNullOrEmpty(answerId))
+			{
+				answerId = this.surveyAnswerStore.GetFirstSurveyAnswerId(tenant, surveySlug);
+			}
 
-          if (!string.IsNullOrEmpty(answerId))
-          {
-            surveyAnswer = this.surveyAnswerStore.GetSurveyAnswer(tenant, surveySlug, answerId);
-          }
+			if (!string.IsNullOrEmpty(answerId))
+			{
+				surveyAnswer = this.surveyAnswerStore.GetSurveyAnswer(tenant, surveySlug, answerId);
+			}
 
-          var surveyAnswerBrowsingContext = this.surveyAnswerStore.GetSurveyAnswerBrowsingContext(tenant, surveySlug, answerId);
+			var surveyAnswerBrowsingContext = this.surveyAnswerStore.GetSurveyAnswerBrowsingContext(tenant, surveySlug, answerId);
 
-          var browseResponsesModel = new BrowseResponseModel
-          {
-            SurveyAnswer = surveyAnswer,
-            PreviousAnswerId = surveyAnswerBrowsingContext.PreviousId,
-            ThisAnswerId = surveyAnswerBrowsingContext.ThisId,
-            NextAnswerId = surveyAnswerBrowsingContext.NextId
-          };
+			var browseResponsesModel = new BrowseResponseModel
+			{
+				SurveyAnswer = surveyAnswer,
+				PreviousAnswerId = surveyAnswerBrowsingContext.PreviousId,
+				ThisAnswerId = surveyAnswerBrowsingContext.ThisId,
+				NextAnswerId = surveyAnswerBrowsingContext.NextId
+			};
 
-          var model = this.CreateTenantPageViewData(browseResponsesModel);
-          model.Title = surveySlug;
-          return this.View(model);
-        }
+			var model = this.CreateTenantPageViewData(browseResponsesModel);
+			model.Title = surveySlug;
+			return this.View(model);
+		}
 
-        public TenantPageViewData<T> CreateTenantPageViewData<T>(T contentModel)
-        {
-            var tenantPageViewData = new TenantPageViewData<T>(contentModel)
-            {
-                Tenant = this.Tenant
-            };
-            return tenantPageViewData;
-        }
-    }
+		public TenantPageViewData<T> CreateTenantPageViewData<T>(T contentModel)
+		{
+			var tenantPageViewData = new TenantPageViewData<T>(contentModel)
+			{
+				Tenant = this.Tenant
+			};
+			return tenantPageViewData;
+		}
+	}
 }
